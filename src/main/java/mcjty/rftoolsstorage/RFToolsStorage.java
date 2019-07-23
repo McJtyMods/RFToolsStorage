97c34ef5e256a77ed7d9999c8e2bff528e413831
McJty <mcjty1@gmail.com>
2019-07-23 19:56:57 +0200

    Initial commit. Empty mod

---
+++ src/main/java/mcjty/rftoolsstorage/RFToolsStorage.java
@@ -0,0 +1,43 @@
+package mcjty.rftoolsstorage;
+
+import mcjty.lib.base.ModBase;
+import mcjty.rftoolsstorage.setup.ModSetup;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(RFToolsStorage.MODID)
+public class RFToolsStorage implements ModBase {
+
+    public static final String MODID = "rftoolspower";
+
+    @SuppressWarnings("PublicField")
+    public static ModSetup setup = new ModSetup();
+
+    public static RFToolsStorage instance;
+
+    public RFToolsStorage() {
+        instance = this;
+
+//        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_CONFIG);
+//        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONFIG);
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener((FMLCommonSetupEvent event) -> setup.init(event));
+
+//        Config.loadConfig(Config.CLIENT_CONFIG, FMLPaths.CONFIGDIR.get().resolve("rftoolsstorage-client.toml"));
+//        Config.loadConfig(Config.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve("rftoolsstorage-common.toml"));
+    }
+
+    @Override
+    public String getModId() {
+        return MODID;
+    }
+
+    public static final String SHIFT_MESSAGE = "<Press Shift>";
+
+
+    @Override
+    public void openManual(PlayerEntity player, int bookIndex, String page) {
+    }
+}
---
+++ src/main/java/mcjty/rftoolsstorage/setup/ModSetup.java
@@ -0,0 +1,28 @@
+package mcjty.rftoolsstorage.setup;
+
+import mcjty.lib.compat.MainCompatHandler;
+import mcjty.lib.setup.DefaultModSetup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+public class ModSetup extends DefaultModSetup {
+
+    public ModSetup() {
+        createTab("rftoolsstorage", () -> new ItemStack(Items.CHEST));
+    }   // @todo
+
+    @Override
+    public void init(FMLCommonSetupEvent e) {
+        super.init(e);
+
+//        RFToolsPowerMessages.registerMessages("rftoolsstorage");
+    }
+
+    @Override
+    protected void setupModCompat() {
+        MainCompatHandler.registerWaila();
+        MainCompatHandler.registerTOP();
+//        FMLInterModComms.sendFunctionMessage("theoneprobe", "getTheOneProbe", "mcjty.rftools.compat.theoneprobe.TheOneProbeSupport");
+    }
+}
