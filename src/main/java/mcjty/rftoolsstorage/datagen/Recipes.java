9776fded00324e443458adc4a0fd6fb844b45b5e
McJty <mcjty1@gmail.com>
2020-04-26 07:59:04 +0200

    Various recipe fixes (for the vanilla recipe book)

--- src/main/java/mcjty/rftoolsstorage/datagen/Recipes.java
+++ src/main/java/mcjty/rftoolsstorage/datagen/Recipes.java
@@ -5,7 +5,6 @@ import mcjty.lib.datagen.BaseRecipeProvider;
 import mcjty.rftoolsbase.modules.various.VariousSetup;
 import mcjty.rftoolsstorage.modules.modularstorage.ModularStorageSetup;
 import mcjty.rftoolsstorage.modules.scanner.StorageScannerSetup;
-import net.minecraft.advancements.criterion.InventoryChangeTrigger;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.ShapedRecipeBuilder;
@@ -23,46 +22,45 @@ public class Recipes extends BaseRecipeProvider {
         add('q', Items.QUARTZ);
         add('Q', Items.QUARTZ_BLOCK);
         add('F', VariousSetup.MACHINE_FRAME.get());
-        group("rftools");
     }
 
     @Override
     protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
         build(consumer, ShapedRecipeBuilder.shapedRecipe(ModularStorageSetup.STORAGE_MODULE0.get())
                         .key('g', Items.GOLD_NUGGET)
-                        .addCriterion("redstone", InventoryChangeTrigger.Instance.forItems(Items.REDSTONE)),
+                        .addCriterion("redstone", hasItem(Items.REDSTONE)),
                 " C ", "gig", "qrq");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(ModularStorageSetup.STORAGE_MODULE1.get())
                         .key('g', Items.GOLD_INGOT)
                         .key('X', ModularStorageSetup.STORAGE_MODULE0.get())
-                        .addCriterion("storage", InventoryChangeTrigger.Instance.forItems(ModularStorageSetup.STORAGE_MODULE0.get())),
+                        .addCriterion("storage", hasItem(ModularStorageSetup.STORAGE_MODULE0.get())),
                 " C ", "gXg", "qrq");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(ModularStorageSetup.STORAGE_MODULE2.get())
                         .key('g', Items.GOLD_BLOCK)
                         .key('X', ModularStorageSetup.STORAGE_MODULE1.get())
-                        .addCriterion("storage", InventoryChangeTrigger.Instance.forItems(ModularStorageSetup.STORAGE_MODULE1.get())),
+                        .addCriterion("storage", hasItem(ModularStorageSetup.STORAGE_MODULE1.get())),
                 " C ", "gXg", "QRQ");
         build(consumer, CopyNBTRecipeBuilder.shapedRecipe(ModularStorageSetup.STORAGE_MODULE3.get())
                         .key('g', Items.DIAMOND_BLOCK)
                         .key('t', VariousSetup.INFUSED_DIAMOND.get())
                         .key('X', ModularStorageSetup.STORAGE_MODULE2.get())
-                        .addCriterion("storage", InventoryChangeTrigger.Instance.forItems(ModularStorageSetup.STORAGE_MODULE2.get())),
+                        .addCriterion("storage", hasItem(ModularStorageSetup.STORAGE_MODULE2.get())),
                 "tCt", "gXg", "QRQ");
 
         build(consumer, ShapedRecipeBuilder.shapedRecipe(ModularStorageSetup.MODULAR_STORAGE.get())
-                        .addCriterion("frame", InventoryChangeTrigger.Instance.forItems(VariousSetup.MACHINE_FRAME.get())),
+                        .addCriterion("frame", hasItem(VariousSetup.MACHINE_FRAME.get())),
                 "rCr", "qFq", "rqr");
         build(consumer, ShapedRecipeBuilder.shapedRecipe(StorageScannerSetup.STORAGE_SCANNER.get())
                         .key('g', Items.GOLD_INGOT)
-                        .addCriterion("frame", InventoryChangeTrigger.Instance.forItems(VariousSetup.MACHINE_FRAME.get())),
+                        .addCriterion("frame", hasItem(VariousSetup.MACHINE_FRAME.get())),
                 "ToT", "gFg", "ToT");
         build(consumer, ShapedRecipeBuilder.shapedRecipe(StorageScannerSetup.STORAGECONTROL_MODULE.get())
                         .key('X', Items.CRAFTING_TABLE)
-                        .addCriterion("ingot", InventoryChangeTrigger.Instance.forItems(Items.IRON_INGOT)),
-                " X ", "rir", " Z ");
+                        .addCriterion("ingot", hasItem(Items.IRON_INGOT)),
+                " X ", "rir", " X ");
         build(consumer, ShapedRecipeBuilder.shapedRecipe(StorageScannerSetup.DUMP_MODULE.get())
                         .key('X', ItemTags.WOODEN_BUTTONS)
-                        .addCriterion("ingot", InventoryChangeTrigger.Instance.forItems(Items.IRON_INGOT)),
-                " X ", "rir", " Z ");
+                        .addCriterion("ingot", hasItem(Items.IRON_INGOT)),
+                " X ", "rir", " X ");
     }
 }
