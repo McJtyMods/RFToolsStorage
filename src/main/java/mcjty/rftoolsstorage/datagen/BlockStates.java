70a59bf433ba5cd4bc0dd404a364c23bebb75ba9
McJty <mcjty1@gmail.com>
2020-09-06 06:59:13 +0200

    Updated to 33.0.37

--- src/main/java/mcjty/rftoolsstorage/datagen/BlockStates.java
+++ src/main/java/mcjty/rftoolsstorage/datagen/BlockStates.java
@@ -13,8 +13,8 @@ import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockModelBuilder;
-import net.minecraftforge.client.model.generators.ExistingFileHelper;
 import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
+import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class BlockStates extends BaseBlockStateProvider {
 
